fbb6dfa08d60430eef450a59fe418a8233053da4
Alexey Ermakov <strangebrew@yandex.ru>
2022-03-13 12:52:25 +0300

    Initial commit

---
+++ api/src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
+const host = process.env.HOST;
+
+app.get("/test", (req, res) => {
+	res.send("Our api server is working correctly");
+})
+
+app.listen(port, () => {
+	console.log("Started api service on port " + port);
+	console.log("On host " + host);
+})
